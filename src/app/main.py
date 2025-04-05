45344bddd419f4bb618e198d9a37e0c5b38fbe38
Leo <leoh0987@gmail.com>
2025-04-04 20:27:18 -0500

    Set Up DB Connection

--- src/app/main.py
+++ src/app/main.py
@@ -1,2 +1,28 @@
-print("Welcome to Nourish Banking Portal!")
+from flask import app, Flask
+from getpass import getpass
+from mysql.connector import connect, Error
 
+
+"""
+TODO: 
+- Set Up Account Registration
+"""
+
+
+if __name__ == "__main__":
+    try:
+        with connect(
+            host="localhost",
+            # user=input("Enter username: "),
+            # password=getpass("Enter password: "),
+             user = 'root', # Remember to change
+             password = "LeoSQLDB#123", # Remember to change
+             database="accounts",
+        ) as connection:
+            initDBQuery = "CREATE DATABASE accounts"
+            with connection.cursor() as cursor:
+                cursor.execute(initDBQuery)
+                for db in cursor:
+                    print(db)
+    except Error as e:
+        print(e)
