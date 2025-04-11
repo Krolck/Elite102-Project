e14f63c39114d793b863a92ec17a119401e9add7
Leo <leoh0987@gmail.com>
2025-04-10 19:30:53 -0500

    Fully Completed account methods

--- src/app/main.py
+++ src/app/main.py
@@ -1,6 +1,13 @@
 from flask import app, Flask
 from getpass import getpass
 from mysql.connector import connect, Error
+import mysql
+
+import mysql.connector
+import mysql.connector.errorcode
+import mysql.vendor
+
+import mysql.vendor.plugin
 
 
 """
@@ -34,7 +41,7 @@ def initDB():
     database = "bank"
         )
     cursor = connection.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS bank")
+    # cursor.execute("CREATE DATABASE IF NOT EXISTS bank")
     cursor.execute(ACCOUNTS_QUERY)
     # cursor.execute("DESCRIBE accounts")
     connection.commit()
@@ -44,10 +51,11 @@ def initDB():
 
 
 
-def getAccount(connection, id):
+def getAccount(connection, id): 
     cursor = connection.cursor()
     cursor.execute(f"SELECT * FROM accounts WHERE id = {id}")
     account = cursor.fetchone()
+    cursor.close()
     return {  
     "ID": account[0],
     "Username" : account[1],
@@ -57,19 +65,36 @@ def getAccount(connection, id):
         }
 
 # REMEMBER to add input sanitization 
-def register(connection):
+def register(connection): # change to "createAccount" later 
     try:
+
         cursor = connection.cursor()
         username = input("Register Username: ")
         password = input("Register Password: ")
-        cursor.execute(f"INSERT INTO accounts ({username}, {password})")
+        cursor.execute("INSERT INTO accounts (username, password) VALUES (%s, %s)", (username, password))
         connection.commit()
         # cursor.execute("SELECT * FROM accounts")
         cursor.close()
-    except Error as e:
-        print("Please Enter Valid Credentials")
-        print(e)
-        register(connection)
+        print(f"Registed {username}")
+    except Error as err:
+        
+        if err.errno == 1062:
+            print("Sorry, this username is taken. Please try a new one.")
+            register(connection)
+        print(err)
+
+def unregister(connection, id): # change to "deleteAccount" later
+    account = getAccount(connection, id)
+    confirm = str.lower(input(f"Are you sure you want to delete {account["Username"]}"))
+    if confirm == "yes":
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM accounts WHERE id = %s", (account["ID"],))
+        connection.commit()
+        cursor.close()
+        print(f"Unregistered {account["Username"]}")
+        
+
+
 
 def login(connection):
     cursor = connection.cursor()
@@ -79,37 +104,70 @@ def login(connection):
         cursor.execute("SELECT * FROM accounts WHERE username = %s AND password = %s", (username, password))
         account = cursor.fetchone()
         cursor.close()
-    
+        
         if account:
             print(f"Successfully logged In! Hello {account[1]}")
             return account[0]
         else:
             print("Incorrect credentials. Please try again.")
             login(connection)
-    except Error as e:
+    except Error as err:
         print("Incorrect credentials. Please try again.")
-        print(e)
+        print(err)
         login(connection)
         
+
+
 
 def deposit(connection, id):
     cursor = connection.cursor()
     account = getAccount(connection, id)
     try:
         amount = float(input("How much would you like to deposit?"))
-        cursor.execute(f"UPDATE accounts SET balance = balance + {amount} WHERE id = {account["ID"]}")
+        cursor.execute("UPDATE accounts SET balance = balance + %s WHERE id = %s", (amount, account["ID"]))
         connection.commit()
         cursor.close()
         account = getAccount(connection, id)
         print(f"Deposited {amount}. Your new balance is {account["Balance"]}.")
-    except ValueError as e:
+    except ValueError as err:
         print("Please Enter a valid amount")
         deposit(connection, id)
 
-def checkBalance(connection, id):
+def withdraw(connection, id):
     cursor = connection.cursor()
     account = getAccount(connection, id)
-    print(account["Balance"])
+    try:
+        amount = float(input("How much would you like to withdraw?"))
+        if amount > account["Balance"]:
+            print(f"You only have {account["Balance"]}. You can't withdraw {amount}!")
+            return
+        cursor.execute("UPDATE accounts SET balance = balance - %s WHERE id = %s", (amount, account["ID"]))
+        connection.commit()
+        cursor.close()
+        account = getAccount(connection, id)
+        print(f"Withdrew {amount}. Your new balance is {account["Balance"]}.")
+    except ValueError as err:
+        print("Please Enter a valid amount")
+
+def modify(connection, id):
+    cursor = connection.cursor()
+    account = getAccount(connection, id)
+    mod = str.lower(input("What would you like the modify"))
+    match mod:
+        case "username":
+            username = input("What would you like your new username to be? ")
+            cursor.execute("UPDATE accounts SET username = %s WHERE id = %s", (username, account["ID"] ))
+            print("Username changed")
+        case "password":
+            password = input("What would you like your new password to be? ")
+            cursor.execute("UPDATE accounts SET password = %s WHERE id = %s", (password, account["ID"]))
+            print("Password changed")
+        case _:
+            print("Invalid Answer")
+
+def checkBalance(connection, id):
+    account = getAccount(connection, id)
+    print(f"Your balance is: {account["Balance"]}.")
 
 
 def printCursor(connection):
@@ -118,18 +176,31 @@ def printCursor(connection):
         print(i)
     cursor.close()
 
+
+
 def main():
     try:
         
         connection = initDB()
-
-        # register(connection)
-
-        id = login(connection)
-
-        deposit(connection, id)
-        checkBalance(connection, id)
-
+        id = None
+        while True:
+            action = str.lower(input("What would you like to do?"))
+            match action:
+                case "register":
+                    register(connection)
+                case "unregister":
+                    unregister(connection, id)
+                case "login":
+                    id = login(connection)
+                case "deposit":
+                    deposit(connection, id)
+                case "check":
+                    checkBalance(connection, id)
+                case "withdraw":
+                    withdraw(connection, id)
+                case _:
+                    print("Invalid Answer")
+                
 
     except Error as e:
         print(e)
